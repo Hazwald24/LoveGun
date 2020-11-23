93280ccb9b7545e3d18ee9f3aa83da2378a39aea
Daniel Butcher <1020078student.sae.edu.au>
2020-11-23 13:08:06 +1100

    Working Checkpoint Doors and Blaster Sound Modified

---
+++ Love Gun/Assets/Our Assets/Door2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door2 : MonoBehaviour
+{
+
+    private bool BotDDead = false;
+    private bool BotEDead = false;
+    private bool BotFDead = false;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("Bot D") == null)
+        { BotDDead = true; }
+        if (GameObject.FindWithTag("Bot E") == null)
+        { BotEDead = true; }
+        if (GameObject.FindWithTag("Bot F") == null)
+        { BotFDead = true; }
+
+        if (BotDDead && BotEDead && BotFDead)
+        { Destroy(GameObject.FindWithTag("Door2")); }
+    }
+
+}
---
+++ Love Gun/Assets/Our Assets/Doors.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Doors : MonoBehaviour
+{
+
+    private bool BotADead = false;
+    private bool BotBDead = false;
+    private bool BotCDead = false;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.FindWithTag("Bot A") == null)
+           { BotADead = true; }
+        if (GameObject.FindWithTag("Bot B") == null)
+        { BotBDead = true; }
+        if (GameObject.FindWithTag("Bot C") == null)
+        { BotCDead = true; }
+
+        if (BotADead && BotBDead && BotCDead)
+        { Destroy(GameObject.FindWithTag("Door1")); }
+    }
+  
+}
